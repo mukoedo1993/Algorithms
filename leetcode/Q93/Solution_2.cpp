5dca46a4f2ce3cd79535376e2044907cd0dd43d7
mukoedo1993 <69023906+mukoedo1993@users.noreply.github.com>
2020-12-09 16:46:23 -0800

    Create Solution_2.cpp

---
+++ leetcode/Q93/Solution_2.cpp
@@ -0,0 +1,39 @@
+//backtracking:
+//Authour: mukoedo1993
+class Solution {
+public:
+    vector<string> restoreIpAddresses(string s) {
+        if(s.size()>12||s.size()<4)return ans;
+        const int sz=s.size();
+        string curr;
+        solve(s,0,curr,sz);
+        return ans;
+    }
+    private:
+    vector<string>ans;
+    void solve(string s, int index,string curr,const int sz_)
+    { const int sz=s.size();
+       
+      if(index==4)
+          return;
+         
+        for(int k=1;k<=3&&k<=sz;k++)
+        {
+            
+            string curr1=curr;
+           
+            string tmp=s.substr(0,k);
+            int i=stoi(tmp);
+            if(to_string(i).size()!=k||i>255)continue;
+          
+             curr=curr+tmp+(index==3?"":".");
+            
+              if(sz_==curr.size()-3&&index==3)
+                 //curr.back()!='.')
+                 ans.push_back(curr);
+              
+            solve(s.substr(k),index+1,curr,sz_);
+             curr=curr1;
+            
+        }
+    }
