cca2f78efe11ada4880433f7b240336c03fd56a9
mukoedo1993 <69023906+mukoedo1993@users.noreply.github.com>
2020-12-15 14:14:50 -0800

    Create Solution1.cpp

---
+++ leetcode/Q5/Solution1.cpp
@@ -0,0 +1,29 @@
+//Author: Huahua
+class Solution {
+public:
+    string longestPalindrome(string s) {
+       const int n=s.size();
+        auto getLen = [&](int l, int r){
+            while(l>=0&&r<n
+                 && s[l]==s[r]){
+                --l;
+                ++r;
+            }
+            return r-l-1;
+        };
+        int len=0;
+        int start=0;
+        for(int i=0;i<n;++i){
+            int cur=max(getLen(i,i),getLen(i,i+1));
+            if(cur>len){
+                len=cur;
+                start=i-(len-1)/2;
+            }
+            
+        }
+        return s.substr(start,len);
+    
+    }
+
+
+};
