5cc915f77338b01849736da7ee3f77973fbc8ad7
mukoedo1993 <69023906+mukoedo1993@users.noreply.github.com>
2020-12-14 18:41:12 -0800

    Update Solution1.cpp

--- leetcode/Q4/Solution1.cpp
+++ leetcode/Q4/Solution1.cpp
@@ -1,8 +1,10 @@
+//Author: mukoedo1993
+//Time:: 48ms
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
       
-        nums1.insert(nums1.begin(),nums2.begin(),nums2.end());
+        nums1.insert(nums1.begin(),nums2.cbegin(),nums2.cend());
         sort(nums1.begin(),nums1.end());
         const int size=nums1.size();
         if(size%2)
