a3b0b771dc12fcb01df3b4546260bde8fd4c136f
mukoedo1993 <69023906+mukoedo1993@users.noreply.github.com>
2020-12-21 03:24:45 -0800

    Update FenWickTree_implementation.cpp

--- Huahua_sp/SP3/FenWickTree_implementation.cpp
+++ Huahua_sp/SP3/FenWickTree_implementation.cpp
@@ -1,3 +1,28 @@
 class FenWickTree{
-
+        
+  public:
+  FenWickTree(int n):sums_(n+1,0){}
+  
+  void update(int i, int delta){
+    while(i<sums_.size()){
+       sums_[i]+=delta;
+        i+=lowbit(i);
+    } 
+  }
+  
+  int query(int i)const{
+  int sum=0;
+    while(i>0){
+      sum+=sums_[i];
+    i-=lowbit(i);
+    }
+    return sum;
+  }
+  
+          
+   private:
+          static inline int lowbit(int x){
+          return x&(-x);
+          }
+          vector<int>sums_;
 };
